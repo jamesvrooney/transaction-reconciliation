70b20f6df1248f8b798b1ff080068176f578a3ae
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-09 23:04:57 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/TransactionReconciliationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReconciliationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionReconciliationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesvrooney/TransactionReconciliationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionReconciliationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
