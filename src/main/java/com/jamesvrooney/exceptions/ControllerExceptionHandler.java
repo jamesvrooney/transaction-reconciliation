9f737126155a4b1a5932ddd40b237644ff29a34d
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-19 15:58:45 +0000

    Replace ResponseEntity with ProblemDetail in Exception handling class.

--- src/main/java/com/jamesvrooney/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/jamesvrooney/exceptions/ControllerExceptionHandler.java
@@ -3,18 +3,19 @@ package com.jamesvrooney.exceptions;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Set;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity handle(ConstraintViolationException constraintViolationException) {
+    public ProblemDetail handle(ConstraintViolationException constraintViolationException) {
         Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
         String errorMessage = "";
 
@@ -28,14 +29,19 @@ public class ControllerExceptionHandler {
             errorMessage = "ConstraintViolationException occurred.";
         }
 
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorMessage);
+
+        return problemDetail;
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity handle(HttpMessageNotReadableException exception) {
+    public ProblemDetail handle(HttpMessageNotReadableException exception) {
 
         String errorMessage = exception.getMessage();
 
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorMessage);
+        problemDetail.setTitle("Invalid input");
+
+        return problemDetail;
     }
 }
